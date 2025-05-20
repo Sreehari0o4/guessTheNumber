3514af411e5384080d2d31fc8592e5f94d386a57
Sreehari0o4 <sreehariskumarwrk01@gmail.com>
2025-05-20 22:42:41 +0530

    Add multiple round feature based on users choice

--- guess.py
+++ guess.py
@@ -1,44 +1,54 @@
 print("\nWelcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.\n")
 
-import random 
-number = random.randint(1,100)
+strt = True
 
-level_text = '''Please select the difficulty level:
-1. Easy (10 chances)
-2. Medium (5 chances)
-3. Hard (3 chances)
-'''
-print(level_text)
+while strt:
+    import random 
+    number = random.randint(1,100)
 
-diff = input("Enter your choice:")
+    level_text = '''Please select the difficulty level:
+    1. Easy (10 chances)
+    2. Medium (5 chances)
+    3. Hard (3 chances)
+    '''
+    print(level_text)
 
-print("")
+    diff = input("Enter your choice:")
 
-match diff:
-    case '1':
-        chances = 10
-        print("Great! You have selected the Easy difficulty level.\n")
-    case '2':
-        chances = 5
-        print("Great! You have selected the Medium difficulty level.\n")
-    case '3':
-        chances = 3
-        print("Great! You have selected the Hard difficulty level.\n")
-    case _:
-        print("Invalid input, defaulting to easy level.\n")
-        chances = 10
+    print("")
 
-for i in range(chances+1):
-    if i == chances:
-        print(f"Sorry! You've used all your chances. The number was {number}.\n")
-        break
-    guess = int(input(f"Enter your guess : "))
-    if guess == number:
-        print(f"Congratulations! You guessed the number in {i+1} attempts\n")
-        break
-    elif guess < number:
-        print(f"Incorrect! The number is greater than {guess} ({chances-i-1} attempts left)\n")
-    else :
-        print(f"Incorrect! The number is less than {guess} ({chances-i-1} attempts left)\n")
+    match diff:
+        case '1':
+            chances = 10
+            print("Great! You have selected the Easy difficulty level.\n")
+        case '2':
+            chances = 5
+            print("Great! You have selected the Medium difficulty level.\n")
+        case '3':
+            chances = 3
+            print("Great! You have selected the Hard difficulty level.\n")
+        case _:
+            print("Invalid input, defaulting to easy level.\n")
+            chances = 10
+
+    for i in range(chances+1):
+        if i == chances:
+            print(f"Sorry! You've used all your chances. The number was {number}.\n")
+            break
+        guess = int(input(f"Enter your guess : "))
+        if guess == number:
+            print(f"Congratulations! You guessed the number in {i+1} attempts\n")
+            break
+        elif guess < number:
+            print(f"Incorrect! The number is greater than {guess} ({chances-i-1} attempts left)\n")
+        else :
+            print(f"Incorrect! The number is less than {guess} ({chances-i-1} attempts left)\n")
+
+    play_again = input("Do you want to play again? (y/n): ").lower()
+    if play_again != 'y':
+        strt = False
+        print("Thank you for playing! Goodbye!")
+    else:
+        print("\nStarting a new game...\n")
 
